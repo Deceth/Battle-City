6dbb5c2156fa0303f4313a7e5504746eada6d647
Vindkast <abiffle@gmail.com>
2008-03-02 22:06:42 +0000

    Moved chat code into CSend::SendMessage and CSend::SendGlobal.  Added "Pressing escape clears and closes chat window".  Commented.

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -8,131 +8,123 @@ const char g_szClassName[] = "BattleCity";
 
 void CreateMyTooltip (HWND hwnd, HINSTANCE ghThisInstance);
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch(msg)
-    {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch(msg) {
+		// Close window
         case WM_CLOSE:
 			Game.running = 0;
 			Sleep(50);
             DestroyWindow(hwnd);
 			break;
+		// Destory window
         case WM_DESTROY:
 			Game.running = 0;
 			Sleep(50);
             PostQuitMessage(0);
 			break;
+		// Focus on window
 		case WM_SETFOCUS:
-		{
 			Game.Input->HasFocus = 1;
-			if (Game.InGame->Minimized == 1)
-			{
+			if (Game.InGame->Minimized == 1) {
 				Game.InGame->Minimized = 0;
-				if (Game.DDraw->FullScreen == 1) Game.DDraw->DirectDraw->SetDisplayMode(Game.ResolutionX, Game.ResolutionY, 16, 0, 0);
+				if (Game.DDraw->FullScreen == 1) {
+					Game.DDraw->DirectDraw->SetDisplayMode(Game.ResolutionX, Game.ResolutionY, 16, 0, 0);
+				}
 				Game.DDraw->LoadSurfaces();
 				Game.DDraw->DirectDraw->RestoreAllSurfaces();
 			}
             break;
-		}
+		// Remove focus from window
 		case WM_KILLFOCUS:
-		{
 			Game.Input->HasFocus = 0;
-        }
+            break;
+		// Any keyboard character
 		case WM_CHAR:
-			if (Game.State == STATE_GAME)
-			{
-				if (Game.InGame->IsChatting == 1)
-				{
-					if (wParam == 13)
-					{
+
+			// If the player is in game,
+			if (Game.State == STATE_GAME) {
+				
+				// If the player is chatting,
+				if (Game.InGame->IsChatting == 1) {
+				
+					// Key: ENTER
+					if (wParam == 13) {
+
+						// Clear out of chat
 						Game.InGame->IsChatting = 0;
-						if (Game.InGame->ChatLine.length() > 0)
-						{
-							if (Game.Player[Game.Winsock->MyIndex]->isAdmin == 2)
-							{
-								std::stringstream parser(Game.InGame->ChatLine);
-								std::string temp;
 
-								parser >> temp;
-								std::cout << temp << std::endl;
+						// If there was text to send,
+						if (Game.InGame->ChatLine.length() > 0) {
 
-								if (strcmp(temp.c_str(), "/g") == 0) //Global message
-								{
-									string tmpString;
-									char SendString[255];
-									memset(SendString, 0, 255);
-									Game.InGame->ChatLine.copy(SendString, tmpString.length() - 3, 3);
-									Game.Winsock->SendData(cmGlobal, SendString);
-									tmpString = Game.Player[Game.Winsock->MyIndex]->Name + ": " + SendString;
-									Game.InGame->AppendChat(tmpString, RGB(255, 165, 0));
-									Game.InGame->ChatLine.clear();
-								}
-								else //Chat Line
-								{
-									char SendString[255];
-									strcpy(SendString, Game.InGame->ChatLine.c_str());
-									if (Game.InGame->HasWalkie == 1)
-										Game.Winsock->SendData(cmWalkie, SendString);
-									else
-										Game.Winsock->SendData(cmChatMessage, SendString);
-									string tmpString;
-									tmpString = Game.Player[Game.Winsock->MyIndex]->Name + ": " + Game.InGame->ChatLine;
-									Game.InGame->AppendChat(tmpString, RGB(255, 165, 0));
-									Game.InGame->ChatLine.clear();
-								}
+							// If GLOBAL and ADMIN,
+							if ((Game.InGame->ChatLine.find("/g",0) == 0) && (Game.Player[Game.Winsock->MyIndex]->isAdmin == 2)) {
+								Game.Send->SendGlobal();
 							}
-							else
-							{
-								char SendString[255];
-								strcpy(SendString, Game.InGame->ChatLine.c_str());
-								if (Game.InGame->HasWalkie == 1)
-									Game.Winsock->SendData(cmWalkie, SendString);
-								else
-									Game.Winsock->SendData(cmChatMessage, SendString);
-								string tmpString;
-								tmpString = Game.Player[Game.Winsock->MyIndex]->Name + ": " + Game.InGame->ChatLine;
-								if (Game.Player[Game.Winsock->MyIndex]->isDead == true)
-								{
-									Game.InGame->AppendChat(tmpString, RGB(135, 135, 135));
-								}
-								else
-								{
-									Game.InGame->AppendChat(tmpString, RGB(0, 255, 0));
-								}
-								Game.InGame->ChatLine.clear();
+
+							// Else, if PM,
+							else if (Game.InGame->ChatLine.find("/pm",0) == 0) {
+								Game.Send->SendWhisper();
 							}
+
+							// Else, NORMAL,
+							else {
+								Game.Send->SendMessage();
+							}
+							// Clear the chat line
+							Game.InGame->ChatLine.clear();
 						}
 					}
-					else
-					{
-						if (wParam == 8)
-						{
-							if (Game.InGame->ChatLine.length() > 1)
-							{
-								Game.InGame->ChatLine.erase(Game.InGame->ChatLine.length() - 1, 1);
-							}
-							else Game.InGame->ChatLine.clear();
+
+
+					// KEY: backspace
+					else if (wParam == 8) {
+						// If there is more than one char, delete the last
+						if (Game.InGame->ChatLine.length() > 1) {
+							Game.InGame->ChatLine.erase(Game.InGame->ChatLine.length() - 1, 1);
 						}
-						else
-						{
-							char blargh[2];
-							blargh[1] = 0;
-							blargh[0] = (char)wParam;
-							if (Game.InGame->ChatLine.length() < 100)
-							{
-								Game.InGame->ChatLine.append(blargh);
-							}
+						// Else, clear the only char in the chatline
+						else {
+							Game.InGame->ChatLine.clear();
+						}
+					}
+
+					// Key: ESCAPE
+					else if (wParam == 27) {
+						// Clear and close the chat
+						Game.InGame->IsChatting = 0;
+						Game.InGame->ChatLine.clear();
+					}
+
+					// KEY: any
+					else {
+						// Append the key to the chat line
+						char blargh[2];
+						blargh[1] = 0;
+						blargh[0] = (char)wParam;
+
+						// TODO: test that the char is alphanumeric
+
+						if (Game.InGame->ChatLine.length() < 100) {
+							Game.InGame->ChatLine.append(blargh);
 						}
 					}
 				}
-				else
-				{
-					if (wParam == 13) Game.InGame->IsChatting = 1;
+				
+				// Else, user is not chatting,
+				else {
 
-					if (wParam == 117 || wParam == 85 && Game.Player[Game.Winsock->MyIndex]->isFrozen == 0) {
+					// KEY: Enter
+					if (wParam == 13) {
+						// Open chat
+						Game.InGame->IsChatting = 1;
+					}
+
+					// KEY: u or F6 (and user isn't frozen)
+					if (((wParam == 85) || (wParam == 117)) && (Game.Player[Game.Winsock->MyIndex]->isFrozen == 0)) {
+						// Get the item under the user's current position
 						CItem *item = Game.Item->findItembyLocation(((int)Game.Player[Game.Winsock->MyIndex]->X + 24) / 48, ((int)Game.Player[Game.Winsock->MyIndex]->Y + 24) / 48);
-						if (item)
-						{
+						// If found, send a command to pick it up
+						if (item) {
 							sCMItem ii;
 							ii.id = item->id;
 							ii.active = item->active;
@@ -140,14 +132,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 							Game.Winsock->SendData(cmItemUp,(char *)&ii,sizeof(ii));
 						}
 					}
-					else if (wParam == 73 || wParam == 105) {
+					// KEY: i or NUMPAD9
+					else if ((wParam == 73) || (wParam == 105)) {
+						// Trigger the info button
 						Game.Input->InfoButton();
 					}
-					else if (wParam == 79 || wParam == 111) {
-						//Drop Orb
+					// KEY: o or NUMPAD/
+					else if ((wParam == 79) || (wParam == 111)) {
+						// Get an orb from the user's inventory
 						CItem *itm = Game.Inventory->findItembyType(5);
-						if (itm)
-						{
+						// If found, send a command to drop it 
+						if (itm) {
 							sCMItem ii;
 							ii.id = itm->id;
 							ii.active = itm->active;
@@ -155,11 +150,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 							Game.Winsock->SendData(cmItemDrop,(char *)&ii,sizeof(ii));
 						}
 					}
-					else if (wParam == 66 || wParam == 98) {
-						//Drop Bomb
+					// KEY: b or NUMPAD2
+					else if ((wParam == 66) || (wParam == 98)) {
+						// Get a bomb from the user's inventory
 						CItem *itm = Game.Inventory->findItembyType(3);
-						if (itm)
-						{
+						// If found, activate it then send a command to drop it
+						if (itm) {
 							sCMItem ii;
 							ii.id = itm->id;
 							ii.active = 1;
@@ -167,43 +163,58 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 							Game.Winsock->SendData(cmItemDrop,(char *)&ii,sizeof(ii));
 						}
 					}
-					else if (wParam == 72 || wParam == 104) {
-						//Use Health Pack
+					// KEY: h or NUMPAD8
+					else if ((wParam == 72) || (wParam == 104) ) {
+						// Get a health pack from the user's inventory
 						CItem *itm = Game.Inventory->findItembyType(2);
-						if (itm)
-						{
+						// If found, send a command to use it
+						if (itm) {
 							Game.Winsock->SendData(cmMedKit, (char *)&itm->id, sizeof(itm->id));
 						}
 					}
-					else if (wParam == 100 || wParam == 68) {//Drop
+					// KEY: b or NUMPAD4 
+					else if ((wParam == 68) || (wParam == 100)) {
+						// Drop the selected item
 						Game.Inventory->Drop();
 					}
-					else if (wParam == 91 || wParam == 123) {//Refresh Data '['
+					// KEY: [
+					else if ((wParam == 123) || (wParam == 91)) {
+						//Refresh Data
 						//Game.Player[Game.Winsock->MyIndex]->timeFrozen = Game.Timer->GetTime() + 60000;
 						//Game.Player[Game.Winsock->MyIndex]->isFrozen = 1;
 					}
-					else if (wParam == 93 || wParam == 125) {//Reload Surfaces ']'
-						if (Game.Tick > Game.Input->lastRefresh)
-						{
-							Game.Input->lastRefresh = Game.Tick + 3000;
+					// KEY: ]
+					else if ((wParam == 93) || wParam == 125) {
+						// Reload Surfaces
+						if (Game.Tick > Game.Input->lastRefresh) {
+							Game.Input->lastRefresh = Game.Tick + TIMER_RELOAD_SURFACES;
 							Game.DDraw->LoadSurfaces();
 							Game.DDraw->DirectDraw->RestoreAllSurfaces();
 							Game.InGame->AppendChat("The game's surfaces were reloaded.", RGB(255, 165, 0));
 						}
 					}
-					else if (wParam == 77 || wParam == 109) {//'m' - Map
+					// KEY: m or NUMPAD-
+					else if ((wParam == 77) || (wParam == 109)) {
+						// Center the player, freeze the player
 						Game.Draw->PlayerOffsetX = 276;
 						Game.Draw->PlayerOffsetY = 276;
-						Game.InGame->ShowMap = 1 - Game.InGame->ShowMap;
 						Game.Player[Game.Winsock->MyIndex]->isMoving = 0;
-						if (Game.InGame->ShowMap == 1) Game.Winsock->SendData(cmMiniMap, " ");
+
+						// Toggle the minimap, and request it if it is now open
+						Game.InGame->ShowMap = 1 - Game.InGame->ShowMap;
+						if (Game.InGame->ShowMap == 1) {
+							Game.Winsock->SendData(cmMiniMap, " ");
+						}
 					}
-					else if (wParam == 80 || wParam == 112) {//'p' - Points
+					// KEY: p or F1
+					else if ((wParam == 80) || (wParam == 112)) {
+						// Trigger the points button
 						Game.Input->PointsButton();
 					}
 				}
 			}
 			break;
+		// Default action: pass the key through to the window
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
