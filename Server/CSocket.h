9842d73c928a768aac88a358767a3700c138de95
Vindkast <abiffle@gmail.com>
2008-03-17 00:20:13 +0000

    commented out TCPPORT for test server

--- Server/CSocket.h
+++ Server/CSocket.h
@@ -4,6 +4,7 @@
 #include "CServer.h"
 
 #define TCPPORT 5643
+//#define TCPPORT 6643
 
 #ifdef WIN32
 #define socklen_t int
@@ -11,35 +12,37 @@
 
 class CServer;
 
-class CSocket
-{
-public:
-	CSocket(CServer *Server);
-	~CSocket();
+class CSocket {
 
-	void InitWinsock();
-	void Cycle();
-	void SendData(int i, unsigned char PacketID, char TheData[255], int len = -1);
-	void SendRaw(int i, char *TheData, int len);
-	int SendAll(int Socket, char *SendString, int SendLength);
-	void ProcessData();
-	void ProcessBuffer(int i);
-	bool hasData(int sock);
+	public:
+		CSocket(CServer *Server);
+		~CSocket();
 
-private:
-	CServer *p;
+		void InitWinsock();
+		void Cycle();
+		void SendData(int i, unsigned char PacketID, char TheData[255], int len = -1);
+		void SendRaw(int i, char *TheData, int len);
+		int SendAll(int Socket, char *SendString, int SendLength);
+		void ProcessData();
+		void ProcessBuffer(int i);
+		bool hasData(int sock);
 
-	void InitTCP();
-	void TCPCycle();
+	private:
+		CServer *p;
 
-    int listener;
-    fd_set master;
-    fd_set read_fds;
-	struct sockaddr_in myaddr; 
-    struct sockaddr_in remoteaddr;
-	socklen_t addrlen;
-	timeval timev;
-protected:
+		void InitTCP();
+		void TCPCycle();
+
+		int listener;
+		fd_set master;
+		fd_set read_fds;
+		struct sockaddr_in myaddr; 
+		struct sockaddr_in remoteaddr;
+		socklen_t addrlen;
+		timeval timev;
+
+	protected:
+
 };
 
 #endif
