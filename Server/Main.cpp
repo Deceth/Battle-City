2989dc8e821d7ab6d718a6e0708cf1d6d7dfa1be
Vindkast <abiffle@gmail.com>
2008-04-13 23:50:51 +0000

    Temporary change to run the server's "while running" loop inside a try/catch block that triggers debug printouts when the server crashes.  Could affect performance (very very slightly), but hopefully will give us more information on what is causing the crashes.

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,4 +1,5 @@
 #include "CServer.h"
+#include <exception> 
 
 CServer Server;
 
@@ -16,68 +17,110 @@ int main(int argc, char *argv[]) {
 	float RespawnTick = 0;
 	float Top20Tick = 0;
 	float CityTick = 0;
-
+	bool printServerInfo = false;
+	stringstream crashMessage;
+	
 	// Initalize the server
 	Server.Init();
 
-	// While the server is running,
-	while (Server.running == 1) {
+	try {
 
-		// Increment the timer values
-		Server.lastTick = Server.Tick;
-		Server.Tick = Server.Timer->GetTime();
+		// While the server is running,
+		while (Server.running == 1) {
 
-		// Tell the server to run its cycle
-		Server.Winsock->Cycle();
+			// Increment the timer values
+			Server.lastTick = Server.Tick;
+			Server.Tick = Server.Timer->GetTime();
 
-		// If the Item timer is up, tell the server to run its Item cycle
-		if (Server.Tick > ItemTick) {
-			Server.Item->cycle();
-			ItemTick = Server.Tick + 100;
-		}
+			// Tell the server to run its cycle
+			Server.Winsock->Cycle();
 
-		// If the Building timer is up, tell the server to run its Building cycle
-		if (Server.Tick > BuildingTick) {
-			Server.Build->cycle();
-			BuildingTick = BuildingTick + 50;
-		}
-
-		// If the Bullet timer is up, tell the server to run its Bullet cycle
-		if (Server.Tick > BulletTick) {
-			Server.TimePassed = Server.Tick - LastBulletTick;
-			Server.Bullet->cycle();
-			LastBulletTick = Server.Tick;
-			BulletTick = Server.Tick + 12;
-		}
-
-		// If the Top20 timer is up, tell the server to run its Top20 cycle
-		if (Server.Tick > Top20Tick) {
-			Server.Account->GenerateTop20();
-			Top20Tick = Server.Tick + 300000;
-		}
-
-		// If the City timer is up, 
-		if (Server.Tick > CityTick) {
-
-			// For each possible city,
-			for (int i = 0; i < MAX_CITIES; i++) {
-
-				// Run the City cycle
-				Server.City[i]->cycle();
+			// If the Item timer is up, tell the server to run its Item cycle
+			if (Server.Tick > ItemTick) {
+				Server.Item->cycle();
+				ItemTick = Server.Tick + 100;
 			}
-			CityTick = Server.Tick + 1000;
+
+			// If the Building timer is up, tell the server to run its Building cycle
+			if (Server.Tick > BuildingTick) {
+				Server.Build->cycle();
+				BuildingTick = BuildingTick + 50;
+			}
+
+			// If the Bullet timer is up, tell the server to run its Bullet cycle
+			if (Server.Tick > BulletTick) {
+				Server.TimePassed = Server.Tick - LastBulletTick;
+				Server.Bullet->cycle();
+				LastBulletTick = Server.Tick;
+				BulletTick = Server.Tick + 12;
+			}
+
+			// If the Top20 timer is up, tell the server to run its Top20 cycle
+			if (Server.Tick > Top20Tick) {
+				Server.Account->GenerateTop20();
+				Top20Tick = Server.Tick + 300000;
+			}
+
+			// If the City timer is up, 
+			if (Server.Tick > CityTick) {
+
+				// For each possible city,
+				for (int i = 0; i < MAX_CITIES; i++) {
+
+					// Run the City cycle
+					Server.City[i]->cycle();
+				}
+				CityTick = Server.Tick + 1000;
+			}
+
+			// If the Respawn timer is up,
+			if (Server.Tick > RespawnTick) {
+
+				// Check for players to respawn, reset the Respawn timer
+				Server.respawnPlayers();
+				RespawnTick = Server.Tick + 1000;
+			}
+
+			// Sleep
+			Server.PlatformSleep(1);
 		}
+	}
 
-		// If the Respawn timer is up,
-		if (Server.Tick > RespawnTick) {
+	// Catch basic exceptions
+	catch (std::exception& e) {
+		crashMessage << endl << endl;
+		crashMessage << "------------------------------" << endl;
+		crashMessage << "SERVER CRASH: EXCEPTION CAUGHT" << endl;
+		crashMessage << "------------------------------" << endl;
+		crashMessage << "Exception details:" << endl;
+		crashMessage << e.what() << endl;
+		crashMessage << "------------------------------" << endl;
 
-			// Check for players to respawn, reset the Respawn timer
-			Server.respawnPlayers();
-			RespawnTick = Server.Tick + 1000;
-		}
+		printServerInfo = true;
+	}
 
-		// Sleep
-		Server.PlatformSleep(1);
+	// Catch everything else
+	catch (...) {
+		crashMessage << endl << endl;
+		crashMessage << "------------------------------" << endl;
+		crashMessage << "SERVER CRASH: no exception :(" << endl;
+		crashMessage << "------------------------------" << endl;
+		printServerInfo = true;
+	}
+
+	// If an exception occurred,
+	if (printServerInfo) {
+		crashMessage << "Server.lastTick: " << Server.lastTick << "\n";
+		crashMessage << "Server.Tick: " << Server.Tick << "\n";
+		crashMessage << "LastBulletTick: " << LastBulletTick << "\n";
+		crashMessage << "ItemTick " << ItemTick << "\n";
+		crashMessage << "BuildingTick: " << BuildingTick << "\n";
+		crashMessage << "BulletTick: " << BulletTick << "\n";
+		crashMessage << "RespawnTick: " << RespawnTick << "\n";
+		crashMessage << "Top20Tick: " << Top20Tick << "\n";
+		crashMessage << "CityTick: " << CityTick << "\n";
+
+		Server.Log->logServerError(crashMessage.str());
 	}
 
 	// If we left the while loop, close the server
